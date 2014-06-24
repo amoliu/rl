e931d903dab2004a8bd92e8781cdf39dcada5ec8
Wouter Caarls <wouter@caarls.org>
2014-06-24 16:21:21 -0300

    Was updating wrong Q value

--- AC/s_ac_pendulum.m
+++ AC/s_ac_pendulum.m
@@ -2,22 +2,22 @@ function [critic, actor, cr] = s_ac_pendulum()
     % Initialize parameters
     actor.alpha   = 0.005;      % Learning rate for the actor
     actor.grids   = 16;         % Total of grid used for the actor
-    actor.tiles   = 3596;     % Total of tiles per grid used for the actor
+    actor.tiles   = 16384;     % Total of tiles per grid used for the actor
     
     critic.alpha  = 0.1;      % Learning rate for the critic
     critic.grids  = 16;       % Total of grid used for the actor
-    critic.tiles  = 3596;   % Total of tiles per grid used for the actor
+    critic.tiles  = 16384;   % Total of tiles per grid used for the actor
     
-    alpha_decay   = 0.75;     % Alpha decay
-    gamma         = 0.77;     % Discount rate
-    lambda        = 0.25;     % Decay rate
+    alpha_decay   = 1;     % Alpha decay
+    gamma         = 0.97;     % Discount rate
+    lambda        = 0.67;     % Decay rate
     memory_size   = 100;      % Eligibility trace memory
     episodes      = 150;      % Total of episodes
     steps         = 100;      % Steps per episode
-    sd            = 0.7;      % Standard-deviation for gaussian noise in action
+    sd            = 1.0;      % Standard-deviation for gaussian noise in action
         
     % Initialize weights for FA
-    critic.weights = ones([critic.grids critic.tiles])*-1000/critic.grids;
+    critic.weights = (ones([critic.grids critic.tiles])*-1000)/critic.grids;
     actor.weights = zeros([actor.grids actor.tiles]);
     
     % Initialize learning curve
@@ -41,7 +41,7 @@ function [critic, actor, cr] = s_ac_pendulum()
         a = normrnd(0, sd);
         [obs, ~, terminal] = env_mops_sim('step', a);
         %norm_obs = normalize(obs, spec.observation_dims, spec.observation_min, spec.observation_max);        
-        norm_obs = obs ./ [ pi/10, pi];
+        norm_old_obs = obs ./ [ pi/10, pi];
 
         if (ee > 10 && cr(ee-1) > -1000)
             critic.alpha = critic.alpha * alpha_decay;
@@ -55,7 +55,7 @@ function [critic, actor, cr] = s_ac_pendulum()
             
             random_u = normrnd(0, sd);
             % Calculate action
-            a = fa_estimate(norm_obs, actor) + random_u;
+            a = fa_estimate(norm_old_obs, actor) + random_u;
             a = max(a, spec.action_min);
             a = min(a, spec.action_max);
             disp([obs a]);
@@ -73,7 +73,7 @@ function [critic, actor, cr] = s_ac_pendulum()
             % Add obs to Eligibility trace
             [~, trace_pos] = min(Z_values);
             Z_values(trace_pos) = 1;
-            Z_obs(trace_pos,:) = GetTiles_Mex(critic.grids, norm_obs, critic.tiles, 1);
+            Z_obs(trace_pos,:) = GetTiles_Mex(critic.grids, norm_old_obs, critic.tiles, 1);
             
             % Don't learn on the first step
             if (tt > 1)
@@ -94,9 +94,9 @@ function [critic, actor, cr] = s_ac_pendulum()
                 
                 % Actor update
                 actor_update = (actor.alpha*random_u*delta)./actor.grids;
-                actor.weights = actor.weights + fa_gradient(norm_obs, actor)*actor_update;
+                actor.weights = actor.weights + fa_gradient(norm_old_obs, actor)*actor_update;
                 
-%                disp([norm_reward delta actor_update max(max(actor.weights))]);
+                %disp([norm_reward delta actor_update max(max(actor.weights))]);
             end
 
             % Prepare for next timestep
