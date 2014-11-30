2c6e3eda842a6b384869b6244ea7acb0aff9df53
Bruno Costa <doravante2@gmail.com>
2014-11-30 16:47:55 -0200

    Tunning the cartpole environment

--- AC/matlab/ac_cartpole_viz.m
+++ AC/matlab/ac_cartpole_viz.m
@@ -4,11 +4,14 @@ function ac_cartpole_viz(actor, cr, rmse)
 
     % Simulation
     % Initialize simulation
-    env_cartpole('init');
+    opts.swingup = 1;
+    env_cartpole('init', opts);
+    
+    norm_factor = [1/4, 1/2, 1/4, 1/7];
     
     % Reset simulation to initial condition
     obs = env_mops_sim('start');
-    %norm_obs = obs ./ [ pi/10, pi];
+    norm_obs = obs ./ norm_factor;
 
     f = figure;
     h = viz_cartpole(obs);
@@ -18,8 +21,9 @@ function ac_cartpole_viz(actor, cr, rmse)
     while terminal == 0
         viz_cartpole(obs, h);
         
-        a = actor.getLLR.query(obs).getMatlabResult;
+        a = actor.getLLR.query(norm_obs).getMatlabResult;
         [obs, ~, terminal] = env_cartpole('step', a);
+        norm_obs = obs ./ norm_factor;
         
         pause(0.05);
     end
--- AC/matlab/sac_cartpole.m
+++ AC/matlab/sac_cartpole.m
@@ -15,32 +15,34 @@ function [critic, actor, cr] = sac_cartpole(varargin)
     args = p.Results;
 
     % Initialize environment
-    spec = env_cartpole('init');
+    opts.swingup = 1;
+    spec = env_cartpole('init', opts);
     
     % Normalization factor used in observations
     norm_factor = [1/4, 1/2, 1/4, 1/7];
+    % norm_factor = [1, 1, 1, 1];
     
     javaSpec = br.ufrj.ppgi.rl.Specification;
 
-    javaSpec.setActorAlpha(0.2);
+    javaSpec.setActorAlpha(0.05);
     javaSpec.setActorMemory(8000);
-    javaSpec.setActorNeighbors(50)
+    javaSpec.setActorNeighbors(30)
     javaSpec.setActorMin(spec.action_min);
     javaSpec.setActorMax(spec.action_max);
-    javaSpec.setActorValuesToRebuildTree(1);
+    javaSpec.setActorValuesToRebuildTree(30);
     
     javaSpec.setCriticInitialValue(0);
-    javaSpec.setCriticAlpha(0.4);
+    javaSpec.setCriticAlpha(0.15);
     javaSpec.setCriticMemory(8000);
-    javaSpec.setCriticNeighbors(40);
-    javaSpec.setCriticValuesToRebuildTree(1);
+    javaSpec.setCriticNeighbors(20);
+    javaSpec.setCriticValuesToRebuildTree(20);
 
     javaSpec.setObservationDimensions(spec.observation_dims);
     javaSpec.setActionDimensions(spec.action_dims);
 
     javaSpec.setLamda(0.65);
     javaSpec.setGamma(0.97);
-    javaSpec.setSd(2.5);  
+    javaSpec.setSd(1);  
     
     agent = br.ufrj.ppgi.rl.ac.StandardActorCritic;
     agent.init(javaSpec);
