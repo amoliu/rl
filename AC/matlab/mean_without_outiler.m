ff03fd494fffab35ec088d0074e12f02084938c6
Bruno Costa <doravante2@gmail.com>
2014-11-30 21:20:12 -0200

    Removing outliers for curve plot

---
+++ AC/matlab/mean_without_outiler.m
@@ -0,0 +1,6 @@
+function m = mean_without_outiler(data)
+    meann = mean(data);
+    stdd = std(data);
+    idx = bsxfun(@lt, meann + 2*stdd, data) | bsxfun(@gt, meann - 2*stdd, data);
+    m = mean(data(~idx));
+end
