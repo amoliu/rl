8f582e2b9eca6ea410a70cee4e0f8ccdbfe248c8
Bruno Costa <doravante2@gmail.com>
2014-12-04 21:48:48 -0200

    Plotting the model using quiver3

--- AC/matlab/plot_model.m
+++ AC/matlab/plot_model.m
@@ -1,8 +1,12 @@
 function plot_model(model)   
     input = model.getLWR.getMatlabDataInput;
     output = model.getLWR.getMatlabDataOutput;
-    
-    diff = input - output;
-    
-    quiverc(input(:,1),input(:,2),diff(:,1),diff(:,2));
+        
+    d = input(:,1:2) - output(:,1:2);
+    for i=1:size(d, 1)
+        d(i,:) = d(i,:) ./ norm(d(i,:));
+    end
+        
+    quiver3(input(:,1),input(:,2),input(:,3),d(:,1),d(:,2),ones(size(output, 1),1));
+    view(45,45);
 end
