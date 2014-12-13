69b68e369afab75a6e24b5ad3cdc46f3f3adf1e7
Bruno Costa <doravante2@gmail.com>
2014-12-13 17:59:30 -0200

    Saving the cr/rmse matrices

--- AC/matlab/avg_mlac_llr.m
+++ AC/matlab/avg_mlac_llr.m
@@ -21,11 +21,13 @@ axis_limits = [0,episodes,-6000,0];
 t = strcat('mlac-', num2str(trials), '-iterations-', num2str(episodes), '-episodes');
 h = errorbaralpha(mean(cr), 1.96.*std(cr)./sqrt(trials), 'Title', t, 'Rendering', 'opaque', 'Axis', axis_limits);
 saveas(h, strcat(path, t), 'png');
+save(strcat(path, t), 'cr');
 
 figure;
 t = strcat('mlac-rmse', num2str(trials), '-iterations-', num2str(episodes), '-episodes');
 h = errorbaralpha(mean(rmse), 1.96.*std(rmse)./sqrt(trials), 'Title', t, 'Rendering', 'opaque');
 saveas(h, strcat(path, t), 'png');
+save(strcat(path, t), 'rmse');
 
 h = figure;
 t = strcat('mlac-', num2str(trials), '-iterations-', num2str(episodes), '-episodes-curves');
@@ -38,4 +40,4 @@ for i=1:trials
     plot(cr(i,:));
 end
 hold off;
-saveas(h, strcat(path, t), 'png');
+%saveas(h, strcat(path, t), 'png');
