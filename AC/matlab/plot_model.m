96140875f02316088fa1d1e9ab926ea9cf4c6bf1
Bruno Costa <doravante2@gmail.com>
2014-12-16 01:34:29 -0200

    Ploting the model by query in random state/actions

--- AC/matlab/plot_model.m
+++ AC/matlab/plot_model.m
@@ -1,4 +1,6 @@
-function plot_model(model)   
+function plot_model(model)
+    %%%%%%%%%%
+    % Memory %
     input = model.getLWR.getMatlabDataInput;
     output = model.getLWR.getMatlabDataOutput;
         
@@ -6,7 +8,104 @@ function plot_model(model)
     for i=1:size(d, 1)
         d(i,:) = d(i,:) ./ norm(d(i,:));
     end
-        
-    quiver3(input(:,1),input(:,2),input(:,3),d(:,1),d(:,2),ones(size(output, 1),1));
-    view(45,45);
+
+    figure;
+    quiver(input(:,1),input(:,2),d(:,1),d(:,2));
+    xlabel('Angle');
+    ylabel('Angular speed');
+    title('Model memory');
+    
+    %%%%%%%%%%%%%%%
+    % Zero action %
+    input = model.getLWR.getMatlabDataInput;
+    output = zeros(size(input ,1), 2);
+    for i=1:size(input ,1)
+        output(i, :) = model.query(input(i, 1:2), 0).getLWRQueryVO.getMatlabResult;
+    end
+    d = input(:,1:2) - output(:,1:2);
+    for i=1:size(d, 1)
+        d(i,:) = d(i,:) ./ norm(d(i,:));
+    end
+    
+    figure;
+    quiver(input(:,1),input(:,2),d(:,1),d(:,2));
+    xlabel('Angle');
+    ylabel('Angular speed');
+    title('Model query using action zero');
+    
+    %%%%%%%%%%%%%%
+    % Min action %
+    input = model.getLWR.getMatlabDataInput;
+    output = zeros(size(input ,1), 2);
+    for i=1:size(input ,1)
+        output(i, :) = model.query(input(i, 1:2), -3).getLWRQueryVO.getMatlabResult;
+    end
+    d = input(:,1:2) - output(:,1:2);
+    for i=1:size(d, 1)
+        d(i,:) = d(i,:) ./ norm(d(i,:));
+    end
+    
+    figure;
+    quiver(input(:,1),input(:,2),d(:,1),d(:,2));
+    xlabel('Angle');
+    ylabel('Angular speed');
+    title('Model query using min action');
+    
+    %%%%%%%%%%%%%%
+    % Max action %
+    input = model.getLWR.getMatlabDataInput;
+    output = zeros(size(input ,1), 2);
+    for i=1:size(input ,1)
+        output(i, :) = model.query(input(i, 1:2), 3).getLWRQueryVO.getMatlabResult;
+    end
+    d = input(:,1:2) - output(:,1:2);
+    for i=1:size(d, 1)
+        d(i,:) = d(i,:) ./ norm(d(i,:));
+    end
+    
+    figure;
+    quiver(input(:,1),input(:,2),d(:,1),d(:,2));
+    xlabel('Angle');
+    ylabel('Angular speed');
+    title('Model query using max action');
+    
+    %%%%%%%%%%%%%%%%%%
+    % Random actions %
+    input = model.getLWR.getMatlabDataInput;
+    output = zeros(size(input ,1), 2);
+    for i=1:size(input ,1)
+        output(i, :) = model.query(input(i, 1:2), rand * 6 - 3).getLWRQueryVO.getMatlabResult;
+    end
+    d = input(:,1:2) - output(:,1:2);
+    for i=1:size(d, 1)
+        d(i,:) = d(i,:) ./ norm(d(i,:));
+    end
+    
+    figure;
+    quiver(input(:,1),input(:,2),d(:,1),d(:,2));
+    xlabel('Angle');
+    ylabel('Angular speed');
+    title('Model query using random actions');
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%
+    % Random state/actions %
+    input = zeros(size(input ,1), 2);
+    for i=1:size(input ,1)
+        input(i, :) = rand(1,2) .* [20 20] - [0 10];
+    end
+    
+    output = zeros(size(input ,1), 2);
+    for i=1:size(input ,1)
+        output(i, :) = model.query(input(i, 1:2), rand * 6 - 3).getLWRQueryVO.getMatlabResult;
+    end
+    d = input(:,1:2) - output(:,1:2);
+    for i=1:size(d, 1)
+        d(i,:) = d(i,:) ./ norm(d(i,:));
+    end
+    
+    figure;
+    quiver(input(:,1),input(:,2),d(:,1),d(:,2));
+    xlabel('Angle');
+    ylabel('Angular speed');
+    title('Model query using random inputs and actions');
 end
