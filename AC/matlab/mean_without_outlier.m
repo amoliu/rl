676303f6ce66423ef4b121511bdfdefbfac7fc98
Bruno Costa <doravante2@gmail.com>
2014-12-15 23:24:50 -0200

    Removing unnecessary file

--- AC/matlab/mean_without_outlier.m
+++
@@ -1,6 +0,0 @@
-function m = mean_without_outlier(data)
-    meann = mean(data);
-    stdd = std(data);
-    idx = bsxfun(@lt, meann + 2*stdd, data) | bsxfun(@gt, meann - 2*stdd, data);
-    m = mean(data(~idx));
-end
