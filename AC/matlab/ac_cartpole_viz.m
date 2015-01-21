c83693f833ef16087a970111f3d2cfde41271608
Bruno Costa <doravante2@gmail.com>
2015-01-20 23:04:44 -0200

    Trying on carpole environment

--- AC/matlab/ac_cartpole_viz.m
+++ AC/matlab/ac_cartpole_viz.m
@@ -1,13 +1,49 @@
-function ac_cartpole_viz(actor, cr, rmse)
+function ac_cartpole_viz(actor, critic, cr)
     figure;
     plot(cr);
-
+   
+    figure;
+    criticInput = critic.getLLR.getMatlabDataInput;
+    criticOutput = critic.getLLR.getMatlabDataOutput;
+    scatter(criticInput(:,1), criticInput(:,2), 25, criticOutput, 'filled')
+    title('Critic');
+    xlabel('Velocity');
+    ylabel('Position');
+    colorbar;
+    
+    figure;
+    actorInput = actor.getLLR.getMatlabDataInput;
+    actorOutput = actor.getLLR.getMatlabDataOutput;
+    scatter(actorInput(:,1), actorInput(:,2), 25, actorOutput, 'filled')
+    title('Actor');
+    xlabel('Velocity');
+    ylabel('Position');
+    colorbar;
+    
+    figure;
+    criticInput = critic.getLLR.getMatlabDataInput;
+    criticOutput = critic.getLLR.getMatlabDataOutput;
+    scatter(criticInput(:,3), criticInput(:,4), 25, criticOutput, 'filled')
+    title('Critic');
+    xlabel('angle[rad]');
+    ylabel('angular velocity[rad/s]');
+    colorbar;
+    
+    figure;
+    actorInput = actor.getLLR.getMatlabDataInput;
+    actorOutput = actor.getLLR.getMatlabDataOutput;
+    scatter(actorInput(:,3), actorInput(:,4), 25, actorOutput, 'filled')
+    title('Actor');
+    xlabel('angle[rad]');
+    ylabel('angular velocity[rad/s]');
+    colorbar;
+    
     % Simulation
     % Initialize simulation
     opts.swingup = 1;
     env_cartpole('init', opts);
     
-    norm_factor = [1/4, 1/2, 1/4, 1/7];
+    norm_factor = [1/12, 1, pi/10, pi];
     
     % Reset simulation to initial condition
     obs = env_mops_sim('start');
