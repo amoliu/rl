1029233a18cdb48e3692f4cbba30cf1beb01878f
Bruno Costa <doravante2@gmail.com>
2015-03-15 18:03:15 -0300

    using new results

--- AC/matlab/curve.m
+++ AC/matlab/curve.m
@@ -4,22 +4,23 @@ clc;
 
 desired_performance = -1000;
 times_in_row = 3;
-power_of_two=8;
+power_of_two=11;
 
 x = linspace(0, power_of_two, power_of_two+1);
 
+folder = '../results/mat/';
+
 % sac dashed line
-load('../results/sac/sac-40-iterations-600-episodes.mat');
+load(strcat(folder, 'sac.mat'));
 [m_sac, c_sac, p_sac] = find_iteration_by_performance(cr, desired_performance, times_in_row);
 disp(strcat('SAC: ', num2str(p_sac * 100), '%'));
 
 % mlac dashed line
-load('../results/mlac/mlac-40-iterations-600-episodes.mat');
+load(strcat(folder, 'mlac.mat'));
 [m_mlac, c_mlac, p_mlac] = find_iteration_by_performance(cr, desired_performance, times_in_row);
 disp(strcat('MLAC: ', num2str(p_mlac * 100), '%'));
 
 % dyna dashed line
-folder = '../results/dyna/';
 m_dyna = zeros(1, power_of_two+1);
 c_dyna = zeros(1, power_of_two+1);
 p_dyna = zeros(1, power_of_two+1);
@@ -28,12 +29,11 @@ m_dyna(1) = m_sac;
 c_dyna(1) = c_sac;
 p_dyna(1) = p_sac;
 for i=1:power_of_two
-    load(strcat(folder, 'dyna-', num2str(2^(i-1)), '-40-iterations-300-episodes.mat'));
+    load(strcat(folder, 'dyna', num2str(i), '.mat'));
     [m_dyna(i+1), c_dyna(i+1), p_dyna(i+1)] = find_iteration_by_performance(cr, desired_performance, times_in_row);
 end
 
 % dyna-mlac dashed line
-folder = '../results/dyna-mlac/';
 m_dyna_mlac = zeros(1, power_of_two+1);
 c_dyna_mlac = zeros(1, power_of_two+1);
 p_dyna_mlac = zeros(1, power_of_two+1);
@@ -42,14 +42,14 @@ m_dyna_mlac(1) = m_mlac;
 c_dyna_mlac(1) = c_mlac;
 p_dyna_mlac(1) = p_mlac;
 for i=1:power_of_two
-    load(strcat(folder, 'dyna-mlac-', num2str(2^(i-1)), '-40-iterations-300-episodes.mat'));
+    load(strcat(folder, 'dyna-mlac', num2str(i), '.mat'));
     [m_dyna_mlac(i+1), c_dyna_mlac(i+1), p_dyna_mlac(i+1)] = find_iteration_by_performance(cr, desired_performance, times_in_row);
 end
 
 %path = make_save_folder('curve');
 h = figure;
 hold on;
-axis_limits = [0,power_of_two,0,250];
+axis_limits = [0,power_of_two,0,350];
 h_sac = errorbaralpha(repmat(m_sac, 1, power_of_two+1), repmat(c_sac, 1, power_of_two+1), 'Rendering', 'alpha', 'Axis', axis_limits, 'Color', 'r');
 h_mlac = errorbaralpha(repmat(m_mlac, 1, power_of_two+1), repmat(c_mlac, 1, power_of_two+1), 'Rendering', 'alpha', 'Axis', axis_limits, 'Color', 'b');
 h_dyna = errorbaralpha(m_dyna, c_dyna, 'Rendering', 'alpha', 'Axis', axis_limits, 'Color', 'g');
