6208a90ef21b89ccf5df02832e3b9eca5b9f40d0
Bruno Costa <doravante2@gmail.com>
2015-03-26 20:07:22 -0300

    Doing it right

--- AC/matlab/ec2/sizes.m
+++ AC/matlab/ec2/sizes.m
@@ -8,37 +8,68 @@ s = socket_client('/home/bruno/Documentos/aws/home.pem');
 restartallservers(s);
 [s,servers] = updateserverlist(s);
 
-episodes = 10;
+episodes = 20;
 step = 2^6;
-trials = 16;
+trials = 15;
 
 cr = zeros(trials, episodes);
 
 actorScale = 0.25;
-for a=1:5
-    criticScale = 0.25;
-    for c=1:5
-        processScale = 0.25;
-        for p=1:5
-            % Do it!
-            for k=1:trials
-               joblist(k).command = codes.dyna_mlac;
-               joblist(k).arguments = {dyna_episodes(step), step, actorScale, criticScale, processScale};
-            end
-            
-            [results,finishtimes]  = runjobs(s,joblist,1);
-
-            % Make .mat file
-            for k=1:trials
-               cr(k,:) = results{k};
-            end
-            filename = strcat('dyna-mlac-', num2str(actorScale), '-', num2str(criticScale), '-', num2str(processScale), '.mat');
-            save(filename, 'cr');
-            
-            %disp(strcat('Actor: ', num2str(actorScale), '. Critic: ', num2str(criticScale), '. Process: ', num2str(processScale)));
-            processScale = processScale * 2;
-        end
-        criticScale = criticScale * 2;
+criticScale = 1;
+processScale = 1;
+for a=1:4
+    for k=1:trials
+       joblist(k).command = codes.dyna_mlac;
+       joblist(k).arguments = {episodes, step, actorScale, criticScale, processScale};
     end
+
+    [results,finishtimes]  = runjobs(s,joblist,1);
+
+    % Make .mat file
+    for k=1:trials
+       cr(k,:) = results{k};
+    end
+    filename = strcat('dyna-mlac-', num2str(actorScale), '-', num2str(criticScale), '-', num2str(processScale), '.mat');
+    save(filename, 'cr');
     actorScale = actorScale * 2;
+end
+
+actorScale = 1;
+criticScale = 0.25;
+processScale = 1;
+for c=1:5
+    for k=1:trials
+       joblist(k).command = codes.dyna_mlac;
+       joblist(k).arguments = {episodes, step, actorScale, criticScale, processScale};
+    end
+
+    [results,finishtimes]  = runjobs(s,joblist,1);
+
+    % Make .mat file
+    for k=1:trials
+       cr(k,:) = results{k};
+    end
+    filename = strcat('dyna-mlac-', num2str(actorScale), '-', num2str(criticScale), '-', num2str(processScale), '.mat');
+    save(filename, 'cr');
+    criticScale = criticScale * 2;
+end
+
+actorScale = 1;
+criticScale = 1;
+processScale = 0.25;
+for p=1:5
+    for k=1:trials
+       joblist(k).command = codes.dyna_mlac;
+       joblist(k).arguments = {episodes, step, actorScale, criticScale, processScale};
+    end
+
+    [results,finishtimes]  = runjobs(s,joblist,1);
+
+    % Make .mat file
+    for k=1:trials
+       cr(k,:) = results{k};
+    end
+    filename = strcat('dyna-mlac-', num2str(actorScale), '-', num2str(criticScale), '-', num2str(processScale), '.mat');
+    save(filename, 'cr');
+    processScale = processScale * 2;
 end
