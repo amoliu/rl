61e597b0e7f1e71568a1ff870543866cb4e8c806
Bruno Costa <doravante2@gmail.com>
2015-04-29 18:12:51 -0300

    Changes on plot

--- AC/matlab/mlac_sac_compare.m
+++ AC/matlab/mlac_sac_compare.m
@@ -4,6 +4,7 @@ clc;
 
 % MLAC
 [critic, actor, cr, rmse, model, actorUpdates, criticUpdates] = mlac_pendulum('episodes', 600, 'verbose', true);
+figure;
 hold on;
 plot(cr, 'r');
 plot(criticUpdates, 'b');
@@ -14,18 +15,23 @@ title('Performance and TD-Error for MLAC on regular environment');
 mlac_cr = cr;
 mlac_cu = criticUpdates;
 mlac_au = actorUpdates;
+hold off;
 
 % SAC
 [critic, actor, cr, actorUpdates, criticUpdates, episodes] = sac_pendulum('episodes', 600, 'verbose', true);
+figure;
 hold on;
 plot(cr, 'r');
-plot(actorUpdates, 'b');
-legend('Performance', 'TD-Error');
+plot(criticUpdates, 'b');
+plot(actorUpdates, 'g');
+legend('Performance', 'TD-Error', 'Actor updates');
 xlabel('Iterations');
 title('Performance and TD-Error for SAC on harder environment');
+hold off;
 
 % Both
-hold on
+figure;
+hold on;
 plot(cr, 'r')
 plot(actorUpdates, 'b')
 plot(mlac_cr, 'k')
@@ -33,3 +39,9 @@ plot(mlac_au', 'g')
 legend('SAC Performance', 'SAC Actor Updates', 'MLAC Performance', 'MLAC Actor Updates')
 title('Comparing SAC and MLAC on a harder environment')
 xlabel('Iterations')
+hold off;
+
+figure;
+hold on;
+plot(abs(mlac_au) - abs(actorUpdates), 'b')
+refline([0 0])
